f9ecbe2aa5f136b6ae5b76d6da760c3d83757c66
Kilnn <kilnn@qq.com>
2024-05-06 10:18:10 +0800

    feat: 添加部分sdk-shenju-adapter的实现

--- sample/app/build.gradle.kts
+++ sample/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         applicationId = "com.topstep.wearkit.sample"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
