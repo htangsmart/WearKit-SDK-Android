d377341a8404a09e0f5510c4f6fef53c922f40a5
liuhua7459 <liuhua7459@163.com>
2024-06-12 13:42:51 +0800

    feat: add media control

--- sample/app/src/main/java/com/topstep/wearkit/sample/ui/notification/MyNotificationListenerService.kt
+++ sample/app/src/main/java/com/topstep/wearkit/sample/ui/notification/MyNotificationListenerService.kt
@@ -4,12 +4,31 @@ import android.annotation.SuppressLint
 import android.app.Notification
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
 import com.topstep.wearkit.sample.MyApplication
 import timber.log.Timber
 
 class MyNotificationListenerService : NotificationListenerService() {
 
     private val wearKit = MyApplication.wearKit
+    private var myMediaController = MyApplication.myMediaController
+
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+        myMediaController?.setNotificationListenerService(this)
+    }
+
+    override fun onListenerDisconnected() {
+        super.onListenerDisconnected()
+        myMediaController?.setNotificationListenerService(null)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        myMediaController.setNotificationListenerService(this)
+        Log.e("aaaa", "myMediaController")
+    }
 
     @SuppressLint("CheckResult")
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
@@ -25,7 +44,7 @@ class MyNotificationListenerService : NotificationListenerService() {
         }
         if (notification.extras != null) {
             val bundle = notification.extras
-             notificationTitle = bundle.getCharSequence(Notification.EXTRA_TITLE).toString()
+            notificationTitle = bundle.getCharSequence(Notification.EXTRA_TITLE).toString()
             notificationContent = bundle.getCharSequence(Notification.EXTRA_TEXT).toString()
         }
         wearKit.notificationAbility.sendAppNotification(
