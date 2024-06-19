38479e35ed73bed26183db156a6ea6f5812cfd9d
Kilnn <kilnn@qq.com>
2024-06-19 16:36:07 +0800

    Sync apis changed

--- sample/app/src/main/java/com/topstep/wearkit/sample/ui/sync/SyncDataActivity.kt
+++ sample/app/src/main/java/com/topstep/wearkit/sample/ui/sync/SyncDataActivity.kt
@@ -6,8 +6,7 @@ import android.os.Bundle
 import android.view.View
 import com.github.kilnn.tool.widget.ktx.clickTrigger
 import com.topstep.wearkit.apis.model.data.WKSyncData
-import com.topstep.wearkit.helper.WKSyncHelper
-import com.topstep.wearkit.helper.model.WKSleepDailySummary
+import com.topstep.wearkit.apis.util.SyncTimeProviderIncremental
 import com.topstep.wearkit.sample.MyApplication
 import com.topstep.wearkit.sample.R
 import com.topstep.wearkit.sample.databinding.ActivitySyncBinding
@@ -16,10 +15,8 @@ import com.topstep.wearkit.sample.entity.*
 import com.topstep.wearkit.sample.ui.base.BaseActivity
 import com.topstep.wearkit.sample.utils.AppUtils
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import timber.log.Timber
-
 
 @SuppressLint("CheckResult")
 class SyncDataActivity : BaseActivity() {
@@ -50,12 +47,17 @@ class SyncDataActivity : BaseActivity() {
 
     private fun initData() {
         viewBind.refreshLayout.setOnRefreshListener {
-            val helper = WKSyncHelper.Builder(wearKit).create()
-            wearKit.deviceAbility.syncData()
+            val device = wearKit.connector.getDevice()?.address
+            if (device.isNullOrEmpty()) {
+                toast("Not Connect Device")
+                return@setOnRefreshListener
+            }
+            val timeProvider = SyncTimeProviderIncremental(this, "sample_sync_time", device)
+            wearKit.deviceAbility.syncData(timeProvider)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe({
-                    saveSyncData(it, helper)
+                    saveSyncData(it, timeProvider)
                 }, {
                     Timber.i(it)
                     toast(R.string.tip_failed)
@@ -67,16 +69,12 @@ class SyncDataActivity : BaseActivity() {
         }
     }
 
-
-    private fun saveSyncData(data: WKSyncData, helper: WKSyncHelper) {
+    private fun saveSyncData(data: WKSyncData, timeProvider: SyncTimeProviderIncremental) {
         when (data.type) {
             // activity
             WKSyncData.Type.ACTIVITY -> {
                 data.toActivity()?.map {
-                    Timber.e(
-                        "Sync Activity-> timestamp:${it.timestampSeconds} steps:${it.steps}  distance:${it.distance} " +
-                                "calories:${it.calories} number:${it.number} duration:${it.duration} sportDuration:${it.sportDuration} "
-                    )
+                    Timber.e("Sync Activity:$it")
                     ActivityEntity(
                         it.timestampSeconds, it.steps, it.distance.toFloat(),
                         it.calories.toFloat(), it.number, it.duration / 60,
@@ -88,18 +86,17 @@ class SyncDataActivity : BaseActivity() {
             }
             // sleep
             WKSyncData.Type.SLEEP -> {
-                helper.collect(data).andThen(
-                    saveSleep(helper)
-                ).subscribe({
-
-                }, {
-                    Timber.i(it)
-                })
+                data.toSleep()?.map {
+                    Timber.e("Sync Sleep:$it")
+                    SleepEntity(it.timestampSeconds, it.duration, it.deep, it.light, it.awake, it.rem, it.nap)
+                }?.let {
+                    appDatabase.sleepDao().insert(it)
+                }
             }
             // heart rate
             WKSyncData.Type.HEART_RATE -> {
                 data.toHeartRate()?.map {
-                    Timber.e("Sync HeartRate-> timestamp:${it.timestampSeconds} value:${it.heartRate}")
+                    Timber.e("Sync HeartRate:$it")
                     HeartRateEntity(it.timestampSeconds, it.heartRate)
                 }?.let {
                     appDatabase.heartRateDao().insert(it)
@@ -108,7 +105,7 @@ class SyncDataActivity : BaseActivity() {
             // blood oxygen
             WKSyncData.Type.BLOOD_OXYGEN -> {
                 data.toBloodOxygen()?.map {
-                    Timber.e("Sync BloodOxygen-> timestamp:${it.timestampSeconds} value:${it.oxygen}")
+                    Timber.e("Sync BloodOxygen:$it")
                     BloodOxygenEntity(it.timestampSeconds, it.oxygen)
                 }?.let {
                     appDatabase.bloodOxygenDao().insert(it)
@@ -117,7 +114,7 @@ class SyncDataActivity : BaseActivity() {
             //blood pressure
             WKSyncData.Type.BLOOD_PRESSURE -> {
                 data.toBloodPressure()?.map {
-                    Timber.e("Sync BloodPressure-> timestamp:${it.timestampSeconds} sbp:${it.sbp} dbp:${it.dbp}")
+                    Timber.e("Sync BloodPressure:$it")
                     BloodPressureEntity(it.timestampSeconds, it.sbp, it.dbp)
                 }?.let {
                     appDatabase.bloodPressureDao().insert(it)
@@ -126,7 +123,7 @@ class SyncDataActivity : BaseActivity() {
             //pressure
             WKSyncData.Type.PRESSURE -> {
                 data.toPressure()?.map {
-                    Timber.e("Sync Pressure-> timestamp:${it.timestampSeconds} value:${it.pressure}")
+                    Timber.e("Sync Pressure:$it")
                     PressureEntity(it.timestampSeconds, it.pressure)
                 }?.let {
                     appDatabase.pressureDao().insert(it)
@@ -135,7 +132,7 @@ class SyncDataActivity : BaseActivity() {
             //sport
             WKSyncData.Type.SPORT -> {
                 data.toSport()?.map {
-                    Timber.e("Sync Sport-> timestamp:${it.timestampSeconds} value:${it.steps}")
+                    Timber.e("Sync Sport:$it")
                     SportEntity(
                         it.timestampSeconds,
                         it.sportType,
@@ -158,27 +155,9 @@ class SyncDataActivity : BaseActivity() {
                     appDatabase.sportDao().insert(it)
                 }
             }
-
             else -> {}
         }
-    }
-
-    private fun saveSleep(helper: WKSyncHelper): Completable {
-        return helper.getSleepChanged().map {
-            getSleepData(it)
-        }.observeOn(AndroidSchedulers.mainThread()).doOnSuccess {
-            appDatabase.sleepDao().insert(it)
-        }.doOnError {
-
-        }.ignoreElement()
-    }
-
-    private fun getSleepData(sleepDataList: List<WKSleepDailySummary>): List<SleepEntity> {
-        val sleepList = mutableListOf<SleepEntity>()
-        sleepDataList.forEach {
-            sleepList.add(SleepEntity(it.timestampSeconds, it.duration, it.deep, it.light, it.awake, it.rem, it.nap))
-        }
-        return sleepList
+        timeProvider.onItemSyncSuccess(data)
     }
 
     private val blockClick: (View) -> Unit = { view ->
@@ -198,11 +177,9 @@ class SyncDataActivity : BaseActivity() {
             viewBind.itemBloodPressure -> {
                 startActivity(Intent(this, BloodPressureActivity::class.java))
             }
-
             viewBind.itemPressure -> {
                 startActivity(Intent(this, PressureActivity::class.java))
             }
-
             viewBind.itemSport -> {
                 startActivity(Intent(this, SportActivity::class.java))
             }
