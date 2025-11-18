b1ac099c943909aceae0060f71dbc03cbfa4a402
zhangweizhuo <aknzwz@qq.com>
2025-11-18 14:40:00 +0800

    feat:Update the demo project to Android 15

--- sample/app/build.gradle.kts
+++ sample/app/build.gradle.kts
@@ -17,12 +17,12 @@ android {
     }
 
     namespace = "com.topstep.wearkit.sample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.topstep.wearkit.sample"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
         multiDexEnabled = true
